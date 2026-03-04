fcd0e90980a6109bd56f9ecb5ce44556c391a6ef
Medicalert <ezba2@gmail.com>
2026-03-04 01:38:57 -0700

    Adjusted structure and began to bring in components (nav)

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './Nav.css';
+
+import menu from '../assets/menu.svg'
+import moped from '../assets/moped.svg'
+import search from'../assets/search.svg'
+
+function Nav(){
+    return(
+        <nav>
+            <a id='home' href="/">welp<img src={moped} alt=""></img></a>
+            <form id="search-wrapper" action="/pages/search.html" method="get" autoComplete="off">
+                <input type="search" placeholder="Search" name="q"></input>
+                <button type="submit" value=""><img className="icon" src={search} alt=""></img></button>
+            </form>
+                
+            <input type="checkbox" id="menu-toggle"/>
+                
+            <label htmlFor="menu-toggle" className="hamburger-icon">
+                <img src={menu} alt=""></img>
+            </label>
+            
+            <div id="nav-wrapper">
+                <a href="/about">About</a>
+                <div className="divider"></div>
+                <a href="/contact">Contact Us</a>
+                <div className="divider"></div>
+                <a href="/feedback">Send Feedback</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
